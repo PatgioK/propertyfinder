55728a0f29171a83ef88a9ed046e561474b59513
PatgioK <patgiok@gmail.com>
2022-04-03 16:04:41 -0700

    login layout + confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    # before_action :configure_permitted_parameters, if :DeviseController? 
+
+    # protected
+
+    # def configure_permitted_parameters
+    #     devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :url])
+    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    # end
 end
