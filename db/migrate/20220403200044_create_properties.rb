da3f8ef4f406ad04ffe31b003ee4c4aede390a62
PatgioK <patgiok@gmail.com>
2022-04-03 13:01:43 -0700

    devise + scaffolds

---
+++ db/migrate/20220403200044_create_properties.rb
@@ -0,0 +1,14 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.integer :rooms
+      t.integer :bathrooms
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
